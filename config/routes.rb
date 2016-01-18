afbba8e4a57d9d34dbd36f23a82576154a63a65b
LauraUmana <laurau1107@gmail.com>
2016-01-18 18:58:59 +0000

    Add quote and simpleform gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Jabber::Application.routes.draw do
   root 'sayings#index'
+
+  resources :sayings
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
