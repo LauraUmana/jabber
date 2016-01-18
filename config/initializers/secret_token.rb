ff007c7dda7554d95580b1afa82a3327d59803e5
LauraUmana <laurau1107@gmail.com>
2016-01-18 15:19:21 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jabber::Application.config.secret_key_base = 'c4b3daf508c4188e890278edb1aa2a4f4272248f2226623da8532a2fe6eb6c776dde3394471c61e9b27c80660183cdcc27604ea74d570bfc07da6c2643adcd6f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jabber::Application.config.session_store :cookie_store, key: '_jabber_session'
