9545bfc0f50661d5eab8798da00ef1f505b689b8
LauraUmana <laurau1107@gmail.com>
2016-01-18 15:34:26 +0000

    Set up hompage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Jabber::Application.routes.draw do
+  root 'sayings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
